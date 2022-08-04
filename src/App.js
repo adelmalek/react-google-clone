316859adc9694ff5928a8c32cc03b739bafead0c
Adel <gerencser.adel@gmail.com>
2022-08-04 17:53:01 +0200

    components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
 
+import { useState } from "react";
+
+import Footer from "./Components/Footer";
+import Navbar from "./Components/Navbar";
+import Routes from "./Components/Routes";
+
 function App() {
+  const [darkTheme, setDarkTheme] = useState(false);
   return (
-    <div className="App">
-      App
+    <div className={darkTheme? "dark" : " "}>
+      <Navbar />
+      <Routes />
+      <Footer />
     </div>
   );
 }
