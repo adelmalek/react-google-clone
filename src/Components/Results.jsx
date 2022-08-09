e114f39dbc09e02fcb36d33065b971d30f1bb1e8
Adel <gerencser.adel@gmail.com>
2022-08-09 14:12:41 +0200

    component Results

--- src/Components/Results.jsx
+++ src/Components/Results.jsx
@@ -9,9 +9,39 @@ export default function Results() {
     const {results, isLoading, searchTerm, getResults} = useResultContext();
     const location = useLocation();
 
-    if (isLoading) return <Loading />;
+    useEffect( () => {
+        getResults("/search/q=Friderikusz Podcast&num=20")
+    }, [])
 
-    return (
-        <div>Results</div>
-    )
+    if (isLoading) {
+        return <Loading />;
+    }
+
+    switch (location.pathname) {
+        case "/search":
+            return (
+                <div className="flex flex-wrap justify-between space-y-6 sm:px-56">
+                    {results?.results?.map( ({ link, title }, index ) => (
+                        <div key={index} className="w-full md:w-2/5">
+                            <a href={link} target="_blank" rel="noreferrer">
+                                <p className="text-sm">
+                                    {link.length > 30 ? link.substring(0, 30) : link}
+                                </p>
+                                <p className="text-lg hover:underline text-blue-700 dark:text-blue-300">
+                                    {title}
+                                </p>
+                            </a>
+                        </div>
+                    ))}
+                </div>
+            )
+        case "/images":
+            return "SEARCH";
+        case "/news":
+            return "SEARCH";
+        case "/videos":
+            return "SEARCH";
+        default:
+            return "ERROR!";
+    }
 }
--- src/Context/ResultContextProvider.js
+++ src/Context/ResultContextProvider.js
@@ -3,7 +3,7 @@ import React, { createContext, useContext, useState } from "react";
 const ResultContext = createContext();
 const baseUrl = 'https://google-search3.p.rapidapi.com/api/v1';
 
-export const ResultContextProvideer = ({ children }) => {
+export const ResultContextProvider = ({ children }) => {
     const [results, setResults] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
     const [searchTerm, setSearchTerm] = useState("");
@@ -11,7 +11,7 @@ export const ResultContextProvideer = ({ children }) => {
     const getResults = async (type) => {
         setIsLoading(true);
 
-        const response = await fetch(`${baseUrl} ${type}`, {
+        const response = await fetch(`${baseUrl}${type}`, {
             method: "GET",
             headers: {
                 'X-User-Agent': 'desktop',
@@ -22,6 +22,8 @@ export const ResultContextProvideer = ({ children }) => {
         })
 
         const data = await response.json();
+
+        console.log(data);
 
         setResults(data);
 
